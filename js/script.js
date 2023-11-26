e6cb7a6e46bcdab197735f6b8f0ec08d6abc4c62
CosimoA <cosimo.archip@gmail.com>
2023-11-26 15:29:53 +0100

    FEATURE: create while cicle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 /* Lista della spesa con ciclo while: Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while. */
+
+
+// Selezionare elemento lista in HTML
+const lista = document.querySelector("lista");
+// Definisco la lista della spesa preesistende 
+const arreySpesa = ["sedano", "carote", "cipolle", "riso basmati", "funghi porcini", "burro", "grana padano"];
+// Scorro la lista della spesa
+let i = 0;
+while (i < arreySpesa.length) {
+    // Mi salvo ogni elemento della lista
+    let alimento = arreySpesa[i];
+    console.log(alimento);
+    i++;
+    // Creo il List Item da inserire nella lista
+
+    // Inserico dentro LI lelemento array salvato
+
+    // Inserco il LI nella UL selezionata ad inizio script
+}
+    
